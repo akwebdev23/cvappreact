057133ddb6bde58ee159a1d08b7a300e75f0c57a
ak <akwebdev23@gmail.com>
2022-04-21 20:17:35 +0300

    App start setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from "react";
+import {BrowserRouter} from "react-router-dom";
+import AppRouter from "./components/AppRouter";
+import Navbar from "./components/Navbar";
 import './App.css';
 
 function App() {
+  const [mobile, setMobile] = useState(false);
+
+  useEffect(()=>{
+    window.onresize = () => {
+      setMobile(window.outerWidth < 576 ? true : false);
+    }
+    let state = window.outerWidth < 576 ? true : false;
+    setMobile(state);
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar mobile={mobile}/>
+      <AppRouter/>
+    </BrowserRouter>
   );
 }
-
 export default App;
