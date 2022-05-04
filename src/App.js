5062949a6bf9af17eb961e211f06f089ea0e3f35
ak <akwebdev23@gmail.com>
2022-05-04 03:21:33 +0300

    App, app router updates

--- src/App.js
+++ src/App.js
@@ -2,10 +2,34 @@ import React, {useEffect, useState} from "react";
 import {BrowserRouter} from "react-router-dom";
 import AppRouter from "./components/AppRouter";
 import Navbar from "./components/Navbar";
+import UserService from "./components/API/UserService";
+
 import './App.css';
+import { useAuthUser } from "./hooks/useAuthUser";
+import { AppContext } from "./hooks/AppContext";
+import { useToggleClass } from "./hooks/useToggleClass";
+import LoadingSpinner from "./components/assets/loadingspinner/LoadingSpinner";
+import LoadingSpinnerCircle from "./components/assets/loadingspinnerCircle/LoadingSpinnerCircle";
 
 function App() {
   const [mobile, setMobile] = useState(false);
+  const [user, setUser] = useState({});
+  const [navToggleClass,  navOpenCloseHandler] = useToggleClass('open');
+  const [authToggleClass,  authOpenCloseHandler] = useToggleClass('open-auth');
+
+  const userData = useAuthUser();
+  const unexpectedAlertData = {
+    messages: 'Непредвиденная ошибка. Попробуйте воспользоваться сервисом позже.',
+    color: 'danger',
+    show: true
+  };
+
+  useEffect(()=>{
+    setUser(userData);
+    console.dir('app use effect userData, user');
+    console.dir(userData);
+    console.dir(user);
+  }, [userData])
 
   useEffect(()=>{
     window.onresize = () => {
@@ -13,13 +37,17 @@ function App() {
     }
     let state = window.outerWidth < 576 ? true : false;
     setMobile(state);
-  })
+  }, [])
   return (
     <BrowserRouter>
-      <div className="header">
-        <Navbar mobile={mobile}/>
-      </div>
-      <div className="main-content"><AppRouter/></div>
+      <AppContext.Provider value={[{user, setUser}, {unexpectedAlertData}, mobile ?  LoadingSpinnerCircle : LoadingSpinner]}>
+        <header className={mobile ? "header "+"mobile":"header"}>
+          <Navbar user={{auth : true}} mobile={mobile} nav={[navToggleClass, navOpenCloseHandler]} auth={[authToggleClass, authOpenCloseHandler]}/>
+        </header>
+        <main className={mobile ? "main-content container mobile":"main-content container px-0"}>
+          <AppRouter />
+        </main>
+      </AppContext.Provider>
     </BrowserRouter>
   );
 }
--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import {Routes, Route} from "react-router-dom";
+import React, { useContext, useEffect, useState } from 'react';
+import {Routes, Route, Redirect, useNavigate, Navigate, useLocation} from "react-router-dom";
 import {publicRoutes, adminRoutes} from '../routes';
+import { AppContext } from '../hooks/AppContext.js';
+import Home from '../pages/Home';
+import Login from '../pages/Login';
+import LoadingSpinner from './assets/loadingspinner/LoadingSpinner';
 
 export default function AppRouter(){
+    const [{user}, {unexpectedAlertData}] = useContext(AppContext);
+    const {pathname}  = useLocation();
+    const [authRoutes, setAuthRoutes] = useState();
+
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+        const routes = adminRoutes.map(({path, Component, ROLES}) => {
+            if(ROLES?.length > 0){
+                if(user.auth === undefined){
+                    return <Route exact key={path} path={path} element={<LoadingSpinner />}/>
+                } else if(!user.auth){
+                    return <Route exact key={path} path={path} element={<Navigate to={'/login'}/>}/>
+                } else if(user?.roles){
+                    if(ROLES.filter(role => user?.roles.includes(role)).length > 0){
+                        return <Route exact key={path} path={path} element={<Component />}/>
+                    } else {
+                        return <Route exact key={path} path={path} element={<div>Нет доступа</div>}/>
+                    }
+                }
+            } else {
+                <Route exact key={path} path={path} element={<Component />}/>
+            }
+        });
+        setAuthRoutes(routes);
+    }, [user]);
     return (
         <Routes>
-            {publicRoutes.map(({path, Component}) => <Route exact key={path} path={path} element={<Component/>}/>)}
-            {adminRoutes.map(({path, Component}) => <Route exact key={path} path={path} element={<Component/>}/>)}
+            {publicRoutes.map(({path, Component}) => <Route exact key={path} path={path} element={<Component />}/>)}
+            {authRoutes?.map((Route) => Route)}
+            <Route exact path={"*"} element={<div>Страница не найдена</div>}/>
         </Routes>
     );
 }
