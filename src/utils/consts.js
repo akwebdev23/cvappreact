6d14eb5680d4017acb2856e57a412bcb973829be
ak <akwebdev23@gmail.com>
2022-05-04 07:49:14 +0300

    production api links on consts

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -7,7 +7,7 @@ export const HOME_ROUTE = '/';
 export const SKILLS_ROUTE = '/skills';
 export const EXPERIENCE_ROUTE = '/experience';
 export const CONTACTS_ROUTE = '/contacts';
-export const API_HOST = 'http://localhost:8000/api';
-export const API_UPLOAD = 'http://localhost:8000/';
+export const API_HOST = 'http://api.akwebd3o.beget.tech/api';
+export const API_UPLOAD = 'http://api.akwebd3o.beget.tech/';
 
 
