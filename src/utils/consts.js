ae5edd6b1f3f82cd96d82f7a602461e7630dfa2e
ak <akwebdev23@gmail.com>
2022-05-05 03:13:22 +0300

    use .env consts

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -7,7 +7,7 @@ export const HOME_ROUTE = '/';
 export const SKILLS_ROUTE = '/skills';
 export const EXPERIENCE_ROUTE = '/experience';
 export const CONTACTS_ROUTE = '/contacts';
-export const API_HOST = 'http://api.akwebd3o.beget.tech/api';
-export const API_UPLOAD = 'http://api.akwebd3o.beget.tech/';
+export const API_HOST = process.env.REACT_APP_API_HOST;
+export const API_UPLOAD = process.env.REACT_APP_API_UPLOAD;
 
 
