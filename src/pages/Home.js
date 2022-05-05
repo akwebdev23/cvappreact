60853e9769c19516d221d58aaed72419e1b2a1d2
ak <akwebdev23@gmail.com>
2022-05-06 01:47:14 +0300

    Main content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 
 import React, {useContext, useEffect} from 'react';
+import { Link } from 'react-router-dom';
 import { AppContext } from '../hooks/AppContext';
 
 function Home() {
@@ -11,8 +12,23 @@ function Home() {
 
   return (
     <div className="home">
-        <h1>CV Main</h1>
-        <div>{JSON.stringify(user.name)}</div>
+        <h2>Добро пожаловать</h2>
+        <div className='d-flex flex-column'>
+          <p className='col-12'>
+            Меня зовут Алим. Я занимаюсь full-stack разработкой веб-сервисов и приложений и безумно влюблен в it и науку в целом. 
+          </p>
+          <p className='col-12'>
+            Я решил создать это приложение в качестве моего портфолио и буду наполнять его по мере развития моей карьеры.
+            Для удобства я разделили его на две части. Мой опыт в виде описания проектов над которыми я работал или работаю и мои Hard Skills, то есть стек технологий с которыми я работал. 
+          </p>
+        </div>
+        <Link to={'/experience'} className='btn btn-success mr-2'>
+          Проекты
+        </Link>
+        <Link to={'/skills'} className='btn btn-info'>
+          Навыки
+        </Link>
+
     </div>
   );
 }
