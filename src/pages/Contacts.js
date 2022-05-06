4f87e749e430815817187ab87d177b7edd701f01
ak <akwebdev23@gmail.com>
2022-05-07 00:37:43 +0300

    Home and contacts pages

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -4,7 +4,15 @@ import React from 'react';
 function Contacts() {
   return (
     <div className="contacts">
-        <h1>Contacts</h1>
+        <h1>Контакты для связи </h1>
+        <div className='contact-icons'>
+          <div className='gmail-icon d-flex'>
+            <a className='nav-link px-0 hover-success' href='mailto:akwebdev23@gmail.com'><img className='pr-1' src="/icons/gmail-logo.png"/>Email: akwebdev23@gmail.com</a>
+          </div> 
+          <div className='telegram-icon d-flex'>
+            <a href="https://t.me/akwebdev23" className='nav-link px-0 hover-success'><img className='img-fluid pr-1' src="/icons/telegram-png.png"/>Telegram: t.me/akwebdev23</a>
+          </div>
+        </div>
     </div>
   );
 }
